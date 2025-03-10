568b63543fa20ffab86ac487843d67ec0aeece40
2kemmz <129622095+2kemmz@users.noreply.github.com>
2025-03-10 20:42:52 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ParadiseCommunity/ParadiseCommunity/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scriptbox.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
