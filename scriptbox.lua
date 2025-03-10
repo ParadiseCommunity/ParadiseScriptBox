92ad63d46f0d2ab6fa8e9a63fc721742685fc4a4
2kemmz <129622095+2kemmz@users.noreply.github.com>
2025-03-10 20:42:19 +0000

    Update and rename ScriptBox.lua to scriptbox.lua

---
+++ scriptbox.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Greenville
+  [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua" -- Fisch
+}
+
+return Games
