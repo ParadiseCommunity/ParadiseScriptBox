499308470c78b9b207c147ffa608cef282bfa70d
2kemmz <129622095+2kemmz@users.noreply.github.com>
2025-03-11 15:54:17 +0000

    Update scriptbox.lua

--- scriptbox.lua
+++ scriptbox.lua
@@ -1,6 +1,6 @@
 local Games = {
   [891852901] = "https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scripts/greenville.lua", -- Greenville
-  [18722757961] = "https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scripts/ragblood.lua" -- Fisch
+  [18722757961] = "https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scripts/ragblood.lua" -- Ragblood
 }
 
 return Games
