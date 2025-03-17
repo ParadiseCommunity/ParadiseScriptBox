d303c681c1975e1a1611fbdf6bc77c22ce52ec2f
2kemmz <129622095+2kemmz@users.noreply.github.com>
2025-03-17 20:48:24 +0000

    Create main.lua

---
+++ scripts/main.lua
@@ -0,0 +1,21 @@
+--Free Scripts
+local scripts = {
+    [16732694052] = "https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scripts/fisch.lua", --Fisch
+    [18722757961] = "https://raw.githubusercontent.com/ParadiseCommunity/ParadiseScriptBox/refs/heads/main/scripts/ragblood.lua", --Ragblood
+}
+
+local gameId = game.PlaceId
+
+if scripts[gameId] then
+    local success, response = pcall(function()
+        return game:HttpGet(scripts[gameId], true)
+    end)
+    
+    if success and response then
+        loadstring(response)()
+    else
+        print("Failed to fetch or execute script for Game ID: " .. gameId)
+    end
+else
+    print("No script found for this Game ID: " .. gameId)
+end
